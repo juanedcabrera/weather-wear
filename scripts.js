2cf29856bc9d75454c47bfc8c69fb3a836ebaacb
juanedcabrera <juanedcabrera@gmail.com>
2023-03-26 11:30:35 -0600

    Initial Styling

--- scripts.js
+++ scripts.js
@@ -8,64 +8,64 @@ const input = searchBox.querySelector("input");
 const button = searchBox.querySelector("button");
 const temperatureElement = document.querySelector(".temperature");
 const descriptionElement = document.querySelector(".description");
+const feelsLikeElement = document.querySelector(".feels-like")
+const humidityElement = document.querySelector(".humidity")
+const windElement = document.querySelector(".wind")
+const locationName = document.querySelector(".location-name")
+const locationRegion = document.querySelector(".location-region")
 
 button.addEventListener("click", () => {
-  clearImage();
-  const location = input.value;
-  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=yes`;
+  getWeatherData();
+});
+
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    getWeatherData();
+  }
+});
+
+function clearWeatherImage() {
+  const image = weatherBox.querySelector("img");
+  if (image) {
+    weatherBox.removeChild(image);
+  }
+}
+
+function getWeatherData() {
+  clearWeatherImage();
+  const locationValue = input.value;
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${locationValue}&aqi=yes`;
 
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      temperature = data.current.temp_f;
-      conditionText = data.current.condition.text;
-      code = data.current.condition.code;
-      console.log(code);
-      textWeather();
+      const location = data.location.name
+      const region = data.location.region
+      const temperature = data.current.temp_f;
+      const conditionText = data.current.condition.text;
+      const feelsLike = data.current.feelslike_f
+      const code = data.current.condition.code;
+      const humidity = data.current.humidity
+      const wind = data.current.wind_mph
+      console.log(data);
+      textWeather(location, region, temperature, conditionText, feelsLike, humidity, wind);
       displayWeatherPic(data);
     })
     .catch((error) => {
       console.error(error);
     });
-});
+}
 
-input.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    clearImage();
-    const location = input.value;
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=yes`;
-
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        temperature = data.current.temp_f;
-        conditionText = data.current.condition.text;
-        code = data.current.condition.code;
-        console.log(code);
-        textWeather();
-        displayWeatherPic(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-});
-function clearImage() {
-    const image = weatherBox.querySelector("img");
-    if (image) {
-      weatherBox.removeChild(image);
-    }
-  }
-
-function textWeather() {
+function textWeather(location, region, temperature, conditionText, feelsLike, humidity, wind) {
+  locationName.textContent = location;
+  locationRegion.textContent = region;
   temperatureElement.textContent = `${temperature}°F`;
   descriptionElement.textContent = conditionText;
+  feelsLikeElement.textContent = `Feels like ${feelsLike}°F`;
+  humidityElement.textContent = `The humidity is ${humidity} percent`
+  windElement.textContent = `Wind chill is ${wind} MPH`
 }
 
-function displayWeatherPic(data) {
-  const iconUrl = `https:${data.current.condition.icon}`;
-  displayImage(iconUrl, 128, 128);
-}
 
 function displayImage(src, width, height) {
   let img = document.createElement("img");
@@ -78,7 +78,7 @@ function displayImage(src, width, height) {
 function displayWeatherPic(data) {
   weatherStates.forEach((state) => {
     if (state.code === data.current.condition.code) {
-      displayImage(state.image, 320, 250);
+      displayImage(state.image, 150, 100);
     }
   });
 }
@@ -87,199 +87,199 @@ function displayWeatherPic(data) {
 const weatherStates = [
   {
     code: 1003,
-    image: "assets/cloudy.png",
+    image: "assets/images/cloudy.png",
   },
   {
     code: 1006,
-    image: "assets/cloudy.png",
+    image: "assets/images/cloudy.png",
   },
   {
     code: 1009,
-    image: "assets/cloudy.png",
+    image: "assets/images/cloudy.png",
   },
   {
     code: 1066,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1069,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1072,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1114,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1117,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1210,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1213,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1216,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1219,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1222,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1225,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1255,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1258,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1279,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1282,
-    image: "assets/snow.png",
+    image: "assets/images/snow.png",
   },
   {
     code: 1087,
-    image: "assets/thunder.png",
+    image: "assets/images/thunder.png",
   },
   {
     code: 1273,
-    image: "assets/thunder.png",
+    image: "assets/images/thunder.png",
   },
   {
     code: 1276,
-    image: "assets/thunder.png",
+    image: "assets/images/thunder.png",
   },
   {
     code: 1000,
-    image: "assets/sunny.png",
+    image: "assets/images/sunny.png",
   },
   {
     code: 1249,
-    image: "assets/hail.png",
+    image: "assets/images/hail.png",
   },
   {
     code: 1204,
-    image: "assets/hail.png",
+    image: "assets/images/hail.png",
   },
   {
     code: 1207,
-    image: "assets/hail.png",
+    image: "assets/images/hail.png",
   },
   {
     code: 1237,
-    image: "assets/hail.png",
+    image: "assets/images/hail.png",
   },
   {
     code: 1252,
-    image: "assets/hail.png",
+    image: "assets/images/hail.png",
   },
   {
     code: 1261,
-    image: "assets/hail.png",
+    image: "assets/images/hail.png",
   },
   {
     code: 1264,
-    image: "assets/hail.png",
+    image: "assets/images/hail.png",
   },
 
   {
     code: 1030,
-    image: "assets/hazy.png",
+    image: "assets/images/hazy.png",
   },
   {
     code: 1135,
-    image: "assets/hazy.png",
+    image: "assets/images/hazy.png",
   },
   {
     code: 1147,
-    image: "assets/hazy.png",
+    image: "assets/images/hazy.png",
   },
   {
     code: 1063,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1150,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1153,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1168,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1171,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1180,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1183,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1186,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1189,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1192,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1195,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1198,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1201,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1240,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1243,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1246,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
   {
     code: 1249,
-    image: "assets/rain.png",
+    image: "assets/images/rain.png",
   },
 ];
